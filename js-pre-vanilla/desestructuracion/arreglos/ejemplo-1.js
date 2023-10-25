f18de391a1094c5c4bbd5c0b4e084c8aec995801
bleesamperio <bleesamperio@outlook.com>
2023-10-24 19:23:37 -0600

    Desestructuracion de arreglo

---
+++ js-pre-vanilla/desestructuracion/arreglos/ejemplo-1.js
@@ -0,0 +1,9 @@
+/**La desestructuracion de arreglos en JS, es una caracteristica  que te permite extraer valores de un arreglo y asignarlos a variables individuales de una manera mas conveniente
+ * 
+ * Asignacion de valores a variables individuales */
+
+const arreglo=[1, 2, 3]
+//Deestructuracion
+let [primerValor, sgundoValor, tercerValor] = arreglo 
+
+console.log(tercerValor);
