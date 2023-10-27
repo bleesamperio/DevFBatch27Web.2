ad764b35a048fa38112d4bbe278be34b5d143b7f
bleesamperio <bleesamperio@outlook.com>
2023-10-26 20:53:50 -0600

    ✨ operador ternario en funcion

---
+++ js-pre-react/operador-ternario/ejemplo-3.js
@@ -0,0 +1,9 @@
+//Retornar un valor en una funcion
+function esPAR(numero){
+    //En base a una condicion de operador ternario, retornar: "Es par" o "Es impar"
+    return (numero % 2 === 0) ? "Es par" : "Es impar"
+}
+
+console.log(esPAR(50));
+
+
