ef4e008fcecc5c7195aad1e8d83d099346e186d4
bleesamperio <bleesamperio@outlook.com>
2023-10-26 20:08:18 -0600

    add operador-ternario

---
+++ js-pre-vanilla/operador-ternario/ejemplo-1.js
@@ -0,0 +1,10 @@
+/**El operador ternario es una forma concisa de escribir una declaracion condicional
+
+Asignar un valor en funcion de que se evalue una condicion*/
+const edad = 19
+
+//Se nos confirme si somos mayores de edad
+//Si eres mayor de 18, eres mayor de edad
+
+const mensaje = (edad >= 18) ? "Eres mayor de edad" : "Eres menor de edad"
+console.log(mensaje);
