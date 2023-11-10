d0fa09038fcfe3cc38c9f94df08206ac05561051
bleesamperio <bleesamperio@outlook.com>
2023-11-09 19:18:41 -0600

    ✅ ToDoList, ToDoItem

---
+++ 03-todo-list/src/Components/ToDoItem.jsx
@@ -0,0 +1,10 @@
+const ToDoItem = ({todo, handleDelete}) => {
+    return (
+      <li>
+          {todo}
+          <button onClick={handleDelete} className='delete-button'>Eliminar</button>
+      </li>
+    )
+  }
+  
+  export default ToDoItem
--- 03-todo-list/src/Components/ToDoList.jsx
+++ 03-todo-list/src/Components/ToDoList.jsx
@@ -1,21 +1,38 @@
 import { useState } from "react"
-
 const ToDoList = () => {
-    const[inputValue, setImputValue] = useState('')
 
-
-
-
-return (
+const handleAdd = ()=>{
+    console.log('agregue:', inputValue);
+}
+const [inputValue, setInputValue] = useState('')
+    return (
     <div>
-        <h1>Lista de tareas</h1>
-        <input 
-        type="text" 
-        value={inputValue} 
-        onChange={(evento)=>setImputValue(evento.target.value)}
-        />
+    <h1>Lista de Tareas</h1>
+    <input
+        type="text"
+        value={inputValue}
+        onChange={(evento) => setInputValue(evento.target.value)}
+    />
+    <button
+        className="add-button" onClick={handleAdd}
+    >
+        Agregar
+    </button>
+    <ul>
+        <li>
+        Item 1
+        <button>Eliminar</button>
+        </li>
+        <li>
+        Item 2
+        <button>Eliminar</button>
+        </li>
+        <li>
+        Item 3
+        <button>Eliminar</button>
+        </li>
+    </ul>
     </div>
 )
 }
-
 export default ToDoList
