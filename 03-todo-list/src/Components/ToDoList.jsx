f9fb2ca5ff0474b1d2b22ce4e09f4d2db91a2d75
bleesamperio <bleesamperio@outlook.com>
2023-11-09 20:23:22 -0600

    ✅ Se agrega la funcion de handleDelete

--- 03-todo-list/src/Components/ToDoList.jsx
+++ 03-todo-list/src/Components/ToDoList.jsx
@@ -1,51 +1,44 @@
 import { useState } from "react"
 import ToDoItem from "./ToDoItem";
-const ToDoList = () => {
-    const [inputValue, setInputValue] = useState('')
-    const [tareas, setTareas] = useState([])
+    const ToDoList = () => {
+        const [inputValue, setInputValue] = useState('')
+        const [tareas, setTareas] = useState([])
 
-    const handleAdd = ()=>{
-        if (inputValue.trim()) {
-            /** metodo push para setTareas 
-            const tareasTemporal = [...tareas]
-            tareasTemporal.push(inputValue)
-            setTareas(setTareas)*/
-            
-            setTareas(...tareas, inputValue)
-            setInputValue('')
+        const handleAdd = ()=>{
+            if (inputValue.trim()) {
+                /** metodo push para setTareas 
+                const tareasTemporal = [...tareas]
+                tareasTemporal.push(inputValue)
+                setTareas(setTareas)*/
+
+                setTareas(...tareas, inputValue)
+                setInputValue('')
+            }
         }
-    }
 
+        const handleDelete = (indiceTarea)=>{
+            setTareas(tareas.filter((tarea, indice)=> indice!== indiceTarea))
+        }
 
-    return (
-    <div>
-    <h1>Lista de Tareas</h1>
-    <input
-        type="text"
-        value={inputValue}
-        onChange={(evento) => setInputValue(evento.target.value)}
-    />
-    <button
-        className="add-button" onClick={handleAdd}
-    >
-        Agregar
-    </button>
-    <ul>
-        <ToDoItem todo='Item Dinamico' handleDelete={(e)=> console.log('Borre item')}/>
-        <li>
-        Item 1
-        <button>Eliminar</button>
-        </li>
-        <li>
-        Item 2
-        <button>Eliminar</button>
-        </li>
-        <li>
-        Item 3
-        <button>Eliminar</button>
-        </li>
-    </ul>
-    </div>
-)
+        return (
+        <div>
+            <h1>Lista de Tareas</h1>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(evento) => setInputValue(evento.target.value)}
+            />
+            <button
+                className="add-button" onClick={handleAdd}
+            >
+                Agregar
+            </button>
+            <ul>
+                {tareas.map((tarea)=>(
+                    <ToDoItem todo={tarea} handleDelete={(e)=> console.log('Borre item')}/>
+                ))}
+            </ul>
+        </div>
+    )
 }
 export default ToDoList
